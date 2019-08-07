379b0027497c60a25a02dd662b3c0b172477e25b
Oscar Gualoto <datacenter@quitec.com.ec>
2019-08-07 17:06:12 -0500

    Pdf services

--- src/app/services/download.service.ts
+++ src/app/services/download.service.ts
@@ -1,19 +1,42 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders,HttpResponse,HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { DocumentoPagi } from "../models/documentoPagi";
 import { GLOBAL } from './global';
 import { Observable } from 'rxjs';
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: 'root'
 })
 export class DownloadService {
-	public url: string;
+  public url: string;
 
-	constructor(public _http: HttpClient) {
-		this.url = GLOBAL.url;
-	}
+  constructor(public _http: HttpClient) {
+    this.url = GLOBAL.url;
+  }
 
-	getPdfDocument(){
-		return this._http.get(this.url + 'download',{responseType: "blob" });
-	}
+  getPdfDocument(pagiSelect: DocumentoPagi[]) {
+    let params = JSON.stringify(pagiSelect);
+    var user = JSON.parse(localStorage.getItem("currentUser"));
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + user.token,
+      'Content-Type': 'application/json',
+    });
+    return this._http.post(this.url + 'download', params, { headers });
+  }
+
+  public downloadReport(pagiSelect: DocumentoPagi[]): Observable<any> {
+    // Create url
+    let url = this.url + 'download';
+    var body = { pagiSelect };
+    var user = JSON.parse(localStorage.getItem("currentUser"));
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + user.token,
+      'Content-Type': 'application/json'
+    });
+
+    return this._http.post(url, body, {
+      responseType: "blob",
+      headers
+    });
+  }
 }
--- src/app/theme/documento/documento.component.ts
+++ src/app/theme/documento/documento.component.ts
@@ -3,128 +3,138 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { DocumentoService } from '../../services/documento.service';
 import { DownloadService } from '../../services/download.service';
-import { Observable, Observer } from 'rxjs';
+import * as FileSaver from "file-saver";
 
 @Component({
-	selector: 'app-documento',
-	templateUrl: './documento.component.html',
-	styleUrls: [ './documento.component.scss' ]
+  selector: 'app-documento',
+  templateUrl: './documento.component.html',
+  styleUrls: ['./documento.component.scss']
 })
 export class DocumentoComponent implements OnInit {
-	loadingPagi = false;
-	loadingView = false;
-	masterSelected: boolean;
+  loadingPagi = false;
+  loadingView = false;
+  masterSelected: boolean;
 
-	error = '';
-	shown = 'hover';
+  error = '';
+  shown = 'hover';
 
-	paginas: DocumentoPagi[];
-	paginaView: DocumentoPagi[];
-	paginaSelect: DocumentoPagi[];
+  paginas: DocumentoPagi[];
+  paginaView: DocumentoPagi[];
+  paginaSelect: DocumentoPagi[];
 
-	constructor(
-		private _route: ActivatedRoute,
-		private _router: Router,
-		private _documentoService: DocumentoService,
-		private _downloadService: DownloadService
-	) {
-		this.masterSelected = false;
-		this.paginas = [];
-		this.getPagiDocumento(this._route.snapshot.paramMap.get('id'));
-	}
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _documentoService: DocumentoService,
+    private _downloadService: DownloadService
+  ) {
+    this.masterSelected = false;
+    this.paginas = [];
+    this.getPagiDocumento(this._route.snapshot.paramMap.get('id'));
+  }
 
-	ngOnInit() {}
+  ngOnInit() { }
 
-	getPagiDocumento(hex: string) {
-		let arcCod = this.hex_to_ascii(hex);
-		this.loadingPagi = true;
-		this._documentoService.getDocumentoPagi(arcCod).subscribe(
-			(response: DocumentoPagi[]) => {
-				if (response === null) {
-					this._router.navigateByUrl('/busqueda');
-				} else {
-					this.listPage(response);
-					this.viewPagi(this.paginas[0].pagId);
-				}
-				this.loadingPagi = false;
-			},
-			(error) => {
-				if (error.error.message === undefined) {
-					this.error = 'Ha ocurrido un error, contacte al administrador del sistema.';
-				} else {
-					this.error = error.error.message;
-				}
-				console.log(error);
-				this.loadingPagi = false;
-			}
-		);
-	}
+  getPagiDocumento(hex: string) {
+    let arcCod = this.hex_to_ascii(hex);
+    this.loadingPagi = true;
+    this._documentoService.getDocumentoPagi(arcCod).subscribe(
+      (response: DocumentoPagi[]) => {
+        if (response === null) {
+          this._router.navigateByUrl('/busqueda');
+        } else {
+          this.listPage(response);
+          this.viewPagi(this.paginas[0].pagId);
+        }
+        this.loadingPagi = false;
+      },
+      (error) => {
+        if (error.error.message === undefined) {
+          this.error = 'Ha ocurrido un error, contacte al administrador del sistema.';
+        } else {
+          this.error = error.error.message;
+        }
+        console.log(error);
+        this.loadingPagi = false;
+      }
+    );
+  }
 
-	hex_to_ascii(hex: string) {
-		var hex = hex.toString();
-		var str = '';
-		for (var n = 0; n < hex.length; n += 2) {
-			str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
-		}
-		return str;
-	}
+  hex_to_ascii(hex: string) {
+    var hex = hex.toString();
+    var str = '';
+    for (var n = 0; n < hex.length; n += 2) {
+      str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+    }
+    return str;
+  }
 
-	listPage(page: DocumentoPagi[]) {
-		this.paginas = page;
-		for (var i = 0; i < this.paginas.length; i++) {
-			this.paginas[i].isSelected = this.masterSelected;
-		}
-	}
+  listPage(page: DocumentoPagi[]) {
+    this.paginas = page;
+    for (var i = 0; i < this.paginas.length; i++) {
+      this.paginas[i].isSelected = this.masterSelected;
+    }
+  }
 
-	viewPagi(_idPagi: number) {
-		this.loadingView = true;
-		this.paginaView = [];
-		this.paginaView.push(this.paginas.find((x) => x.pagId == _idPagi));
-		this.loadingView = false;
-	}
+  viewPagi(_idPagi: number) {
+    this.loadingView = true;
+    this.paginaView = [];
+    this.paginaView.push(this.paginas.find((x) => x.pagId == _idPagi));
+    this.loadingView = false;
+  }
 
-	selectPagi(_idPagi: number) {
-		this.loadingView = true;
-		this.paginaView = [];
-		this.paginaView.push(this.paginas.find((x) => x.pagId == _idPagi));
-		this.loadingView = false;
-	}
+  selectPagi(_idPagi: number) {
+    this.loadingView = true;
+    this.paginaView = [];
+    this.paginaView.push(this.paginas.find((x) => x.pagId == _idPagi));
+    this.loadingView = false;
+  }
 
-	checkUncheckAll() {
-		for (var i = 0; i < this.paginas.length; i++) {
-			this.paginas[i].isSelected = this.masterSelected;
-		}
-		this.getCheckedItemList();
-	}
+  checkUncheckAll() {
+    for (var i = 0; i < this.paginas.length; i++) {
+      this.paginas[i].isSelected = this.masterSelected;
+    }
+    this.getCheckedItemList();
+  }
 
-	isAllSelected() {
-		this.masterSelected = this.paginas.every(function(item: any) {
-			return item.isSelected == true;
-		});
-		this.getCheckedItemList();
-	}
+  isAllSelected() {
+    this.masterSelected = this.paginas.every(function (item: any) {
+      return item.isSelected == true;
+    });
+    this.getCheckedItemList();
+  }
 
-	getCheckedItemList() {
-		this.paginaSelect = [];
-		for (var i = 0; i < this.paginas.length; i++) {
-			if (this.paginas[i].isSelected) this.paginaSelect.push(this.paginas[i]);
-		}
-		console.log(this.paginaSelect);
-		//this.checkedList = JSON.stringify(this.checkedList);
-	}
+  getCheckedItemList() {
+    this.paginaSelect = [];
+    for (var i = 0; i < this.paginas.length; i++) {
+      if (this.paginas[i].isSelected) this.paginaSelect.push(this.paginas[i]);
+    }
+  }
 
-	dowdownload() {
-		this._downloadService.getPdfDocument().subscribe(
-			(response) => {
-				//console.log(response);
-				var file = new Blob([response], {type: 'application/pdf'});
-				var fileURL = URL.createObjectURL(file);
+  dowdownload() {
+    this._downloadService.downloadReport(this.paginaSelect).subscribe(
+      data => {
+        //saveAs(data, filename);
+        console.log(data);
+      },
+      err => {
+        alert("Problem while downloading the file.");
+        console.error(err);
+      }
+    );
+    // this._downloadService.getPdfDocument(this.paginaSelect).subscribe(
+    //   (response) => {
+    //     //this.downLoadFile(response, 'WeeklySummary.pdf');
+    //     console.log(response);
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //   }
+    // );
+  }
 
-				console.log(file);
-			},
-			(error) => {
-				console.log(error);
-			}
-		);
-	}
+  downLoadFile(data: any, type: string) {
+    var blob = new Blob([data], { type: "application/pdf" });
+    FileSaver.saveAs(blob, "WeeklySummary.pdf");
+  }
 }
